b1138453e01badd4b82e38b2d7b15a89ed84f37b
alejuran <alejandrourn@gmail.com>
2022-08-23 17:28:08 -0700

    advanced puppet ssh config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,5 @@
+# Client configuration file
+exec { 'ssh_config':
+  path    => '/bin',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+}
