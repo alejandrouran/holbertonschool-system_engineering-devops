50cc17bcf9b5722b4bb08cd3b7557c1d16210fed
alejuran <alejandrourn@gmail.com>
2022-08-27 14:50:01 -0700

    2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,17 @@
+# Add a custom HTTP header with Puppet 
+exec { '/usr/bin/env apt-get -y update' : }
+-> package { 'nginx':
+  ensure => installed,
+}
+-> file { '/var/var/html/index.html' :
+  content => 'Holberton School!',
+}
+-> file_line {'add header' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
+}
+-> service { 'nginx':
+  ensure => runnig,
+}
