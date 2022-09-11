8c5ad27d8ced1554603b67db9a66cb85d2d4bf6d
alejuran <alejandrourn@gmail.com>
2022-09-11 16:09:14 -0500

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,6 +4,7 @@ queries the Reddit API and returns
 the number of subscribers
 """
 
+import json
 import requests
 
 
@@ -12,8 +13,12 @@ def top_ten(subreddit):
     ...
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    head = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    params = {"limit": 10}
+    head = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
 
     r = requests.get(url, headers=headers, params=params,
                      allow_redirects=False)
